0cf40f4f38dd7f7e2613bd4c33c49fec92224604
David Cole <sommnus@gmail.com>
2017-07-18 11:00:01 -0500

    Updated script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 ln -s ~/.commonconfig/Xresources ~/.Xresources
 ln -s ~/.commonconfig/vimrc ~/.vimrc
+curl https://raw.githubusercontent.com/mina86/urxvt-tabbedex/master/install | sh
+xrdb ~/.Xresources
